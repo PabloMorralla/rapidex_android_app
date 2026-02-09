de5c77fa6d4287d4ade561b5f21df2719f7322ad
Pablo Morralla <pabmordel2@alu.edu.gva.es>
2026-02-09 19:46:27 +0100

    DONE: NavBar & Pager

---
+++ app/src/main/java/com/rapidex/rapidex_android_app/ui/components/MainBottomNavBar.kt
@@ -0,0 +1,31 @@
+package com.rapidex.rapidex_android_app.ui.components
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.rapidex.rapidex_android_app.ui.main.viewmodel.MainDestination
+
+@Composable
+fun MainBottomNavBar (
+    entries: List<MainDestination>,
+    currentPageIndex: Int,
+    onNavigate: (Int)->Unit
+) {
+    NavigationBar {
+        entries.forEachIndexed { i, entry ->
+            NavigationBarItem(
+                selected = (currentPageIndex == i),
+                onClick = { onNavigate(i) },
+                icon = {
+                    Icon(
+                        imageVector = entry.icon,
+                        contentDescription = entry.title
+                    )
+                },
+                label = { Text(entry.title) }
+            )
+        }
+    }
+}
